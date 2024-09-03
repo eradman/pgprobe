8c7cdc7165f103f23bced0619c30158dc50181f5
Eric Radman <ericshane@eradman.com>
2024-09-03 10:15:43 -0400

    Move sources to top-level directory

--- smoketest.sh
+++ smoketest.sh
@@ -10,9 +10,9 @@ function log {
 
 log "starting test database"
 url=$(pg_tmp)
-psql="psql -P footer=off -P linestyle=unicode --no-psqlrc -q -v ON_ERROR_STOP=1 $url"
-$psql -f schema/roles.sql
-$psql -At <<-SQL
+alias psql="psql -P footer=off -P linestyle=unicode --no-psqlrc -q -v ON_ERROR_STOP=1 $url"
+psql -f schema/roles.sql
+psql -At <<-SQL
 	SELECT setting || '/postgres.log' AS logfile
 	FROM pg_settings
 	WHERE name = 'data_directory'
@@ -20,15 +20,15 @@ SQL
 
 log "loading schema"
 for sql in $(ls -d schema/0*); do
-	$psql -f $sql -o /dev/null
+	psql -f $sql -o /dev/null
 done
-$psql <<-SQL
+psql <<-SQL
 	INSERT INTO probe_rules (name, url, remain_idle, active)
 	VALUES ('pg_tmp', '${url}&application_name=probe_test', 2, 't');
 SQL
 
 log "starting pgprobe"
-src/pgprobe $url &
+./pgprobe $url &
 sleep 0.5
 
 log "show process tree"
@@ -36,7 +36,7 @@ pstree $!
 
 log "terminating a single connection"
 sleep 0.5
-$psql <<-SQL
+psql <<-SQL
 	SELECT pid,state,application_name,pg_terminate_backend(pid) FROM pg_stat_activity
 	WHERE usename is not null
 	AND application_name = 'probe_test'
@@ -45,7 +45,7 @@ SQL
 
 log "report events"
 sleep 4
-$psql <<SQL
+psql <<SQL
 	SELECT event_time,in_recovery,connect_time,query_time,error_message,check_step
 	FROM response_log
 	ORDER BY event_time;
@@ -53,7 +53,7 @@ SQL
 
 log "Deactive rules to trigger reload/abort"
 sleep 4
-$psql -c "UPDATE probe_rules SET active='f'"
+psql -c "UPDATE probe_rules SET active='f'"
 
 wait
 log "OK"
